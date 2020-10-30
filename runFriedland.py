d8066cd1fee5b998af6faa763f050bb2e827ebdd
Yun-Tse Tsai <yuntsebaryon@gmail.com>
2020-10-29 19:40:50 -0700

    run SNOWGloBES and make plots comparing input and output fluxes

---
+++ runFriedland.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+def makePlot( flux, arrivedFile, detectedFile, oDir ):
+    arrived = np.genfromtxt( arrivedFile )
+    detected = np.genfromtxt( detectedFile, skip_footer=2 )
+    plt.rc( 'xtick', labelsize = 20 )
+    plt.rc( 'ytick', labelsize = 20 )
+    plt.plot( arrived[:,0], arrived[:,1], linewidth = 2, label = 'Arrived' )
+    plt.plot( detected[:,0], detected[:,1]*1e7, linewidth = 2, label = 'Detectedx1e7' )
+    plt.xlabel( 'Energy [GeV]', fontsize = 20 )
+    plt.ylabel( 'Event Rate', fontsize = 20 )
+    plt.legend()
+    plt.tight_layout()
+    outfile = oDir + '/' + flux + '.pdf'
+    plt.savefig( outfile )
+    plt.close()
+
+# def makePlot()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser( description = 'Run SNOwGLoBES on Alex Friedland supernova neutrino fluxes and plot them' )
+    parser.add_argument( '-s', dest = 'sDir', type = str, help = 'the location of SNOwGLoBES' )
+    parser.add_argument( '-o', dest = 'oDir', type = str, help = 'the directory of the output plots' )
+    parser.add_argument( '-c', dest = 'dConfig', type = str, help = 'the detector resolution/smearing, default = ar40kt_DCR_THR35QENonRefl2PE', default = 'ar40kt_DCR_THR35QENonRefl2PE' )
+
+    args = parser.parse_args()
+
+    files = glob.glob( args.sDir + '/fluxes/rf*' )
+
+    fluxes = []
+
+    for file in files:
+        file = os.path.split(file)[-1]
+        if file.endswith( '.dat' ): file = file[:-len('.dat')]
+        fluxes.append( file )
+
+    # Run SNOwGLoBES
+    for flux in fluxes:
+        cmd = './supernova.pl ' + flux + ' argon_marley1_ar ' + args.dConfig
+        # os.system( cmd )
+
+    if not os.path.exists( args.oDir ):
+        os.mkdir( args.oDir )
+
+    # Make flux plots
+    for flux in fluxes:
+        infile = args.sDir + '/fluxes/' + flux + '.dat'
+        outfile = args.sDir + '/out/' + flux + '_nue_Ar40_marley1_' + args.dConfig + '_events_smeared.dat'
+        makePlot( flux, infile, outfile, args.oDir )
